25cb89f3d820b56f31be3f0390e0d163973eaa01
Quentin Carlin <quentincarlin@Quentins-MacBook-Air.local>
2018-11-13 10:45:16 -0500

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontComponent } from './front/front.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'front',
+    component: FrontComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontComponent } from './front/front.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontComponent
+    FrontComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
